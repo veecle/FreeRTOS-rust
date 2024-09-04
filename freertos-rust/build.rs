bc732f6a48d58779cdc62823524cbf9b21b1c541
arctic-alpaca <67190338+arctic-alpaca@users.noreply.github.com>
2024-09-04 16:32:17 +0200

    feat: explicitly link freeRTOS lib

--- freertos-rust/build.rs
+++ freertos-rust/build.rs
@@ -4,6 +4,8 @@ use std::path::PathBuf;
 // See: https://doc.rust-lang.org/cargo/reference/build-scripts.html
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    // Ensures the freeRTOS library is linked explicitly.
+    println!("cargo::rustc-link-lib=static:-bundle=freertos");
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     println!(
         "cargo:SHIM={}",
